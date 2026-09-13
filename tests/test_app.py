9948b1acd648c7207439e8c534b4006b2609359a
Katakam-Krupavathi <krupavathikatakam2006@gmail.com>
2026-09-13 21:06:11 +0530

    Rename application.py to app.py and update WSGI configuration and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ import pytest
 # Ensure project root is in python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from application import app
+from app import app
 
 @pytest.fixture
 def client():
